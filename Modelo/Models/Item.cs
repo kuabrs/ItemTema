e96ad17b97a17bbf5280e0c35ef30d90d9b29029
KAUÃ BATISTA RIBEIRO DE SOUZA <kaua.batista@academico.ifrn.edu.br>
2023-12-29 10:23:57 -0300

    .

--- Modelo/Models/Item.cs
+++ Modelo/Models/Item.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ItemTema.Models
+namespace Modelo.Models
 {
     public class Item
     {
--- Persistencia/DAL/ItemDAL.cs
+++ Persistencia/DAL/ItemDAL.cs
@@ -6,7 +6,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
-namespace ItemTema.Persistencia.DAL
+namespace Persistencia.DAL
 {
     public class ItemDAL
     {
