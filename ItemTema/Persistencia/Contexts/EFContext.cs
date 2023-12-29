c6ba2ff8fa7ad467bc9cbb9bd71e15ac47891e70
KAUÃ BATISTA RIBEIRO DE SOUZA <kaua.batista@academico.ifrn.edu.br>
2023-12-29 02:07:52 -0300

    progresso

---
+++ ItemTema/Persistencia/Contexts/EFContext.cs
@@ -0,0 +1,21 @@
+﻿using ItemTema.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ItemTema.Contexts
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+        public DbSet<Item> Items { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ItemTema/Persistencia/DAL/ItemDAL.cs
@@ -0,0 +1,42 @@
+﻿using ItemTema.Contexts;
+using ItemTema.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ItemTema.Persistencia.DAL
+{
+    public class ItemDAL
+    {
+        private EFContext context = new EFContext();
+        public IQueryable<Item> ObterItemsClassificadosPorNome()
+        {
+            return context.Items.OrderBy(b => b.Nome);
+        }
+        public Item ObterItemPorId(long id)
+        {
+            return context.Items.Where(f => f.ItemId == id).First();
+        }
+        public void GravarItem(Item Item)
+        {
+            if (Item.ItemId == 0)
+            {
+                context.Items.Add(Item);
+            }
+            else
+            {
+                context.Entry(Item).State = EntityState.Modified;
+            }
+            context.SaveChanges();
+        }
+        public Item EliminarItemPorId(long id)
+        {
+            Item Item = ObterItemPorId(id);
+            context.Items.Remove(Item);
+            context.SaveChanges();
+            return Item;
+        }
+    }
+}
